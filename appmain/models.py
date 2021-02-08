d0a4943a1a363f0e1a9bc17ac26a9ea63a961ea0
Sathish-Kanna <mskanna95@gmail.com>
2021-02-08 23:01:07 +0530

    assigned suitable data field

--- appmain/models.py
+++ appmain/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class KeyModel(models.Model):
     voter_id = models.CharField(max_length=10, blank=True)
-    temp_id = models.TextField(max_length=256, blank=True)
-    pukey = models.CharField(max_length=512, blank=True)
+    temp_id = models.CharField(max_length=256, blank=True)
+    pukey = models.TextField(max_length=512, blank=True)
